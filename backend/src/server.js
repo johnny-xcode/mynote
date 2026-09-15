ce61b856582202483b6166652ae655971911edb4
jhontab <jhontab@gmail.com>
2026-09-15 22:45:28 +0800

    deployment adjustments

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,21 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-// connectDB();
-app.use(cors({origin: "http://localhost:5173"})); // Enable CORS for all routes
+
+if(process.env.NODE_ENV !== "production")
+{
+      app.use(cors({origin: "http://localhost:5173"})); // Enable CORS for all routes
+}
+
+
 app.use(express.json()); // middleware to parse JSON request bodies
 
 app.use(rateLimiter); // Apply the rate limiter middleware to all routes
@@ -24,6 +31,17 @@ app.use(rateLimiter); // Apply the rate limiter middleware to all routes
 
 
 app.use('/api/notes', notesRoutes);
+
+
+if(process.env.NODE_ENV === "production")
+{
+   app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+   app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname,"../frontend/","dist","index.html"));
+   });
+}
+
 
 connectDB().then(() => {
       app.listen(PORT, () => {
